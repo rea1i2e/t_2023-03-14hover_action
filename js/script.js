31c0bbc7ad632f8e738f1e11383de62b9bf63172
Yoshiaki Numajiri <afmaar128@gmail.com>
2023-03-14 17:18:28 +0900

    ホバーの挙動は上手くいっているが、リロード時につらつく

---
+++ js/script.js
@@ -0,0 +1,28 @@
+jQuery(function ($) {
+  const classActive ='is-active';
+  const $js_category_item = $(".js-category-item"); // カテゴリ
+  const $js_default_category_item = $(".js-category-item[data-category='category1']"); // デフォルトカテゴリ
+  const $js_category_item_target = $(".js-category-item-target"); // イメージ
+  const $js_default_category_item_target = $(".js-category-item-target[data-target='category1']"); // デフォルトイメージ
+  
+  // カテゴリーメニューにホバーしたら
+  $js_category_item.hover(
+    function () {
+      const this_category = $(this).data("category"); // ホバーされたカテゴリのdata-categoryの属性値を取得
+      $js_category_item.removeClass(classActive); // すべてのカテゴリーから.is-active削除
+      $(this).addClass(classActive); // ホバー中のカテゴリーに.is-active追加
+      $js_category_item_target.removeClass(classActive); // すべてのイメージから.is-active削除
+      $js_category_item_target.filter(`[data-target=${this_category}]`).addClass(classActive); // ホバー中のカテゴリーに対応するイメージに.is-active追加
+    },
+    function () {
+      $js_category_item.removeClass(classActive);
+      $js_default_category_item.addClass(classActive);
+      $js_category_item_target.removeClass(classActive);
+      $js_default_category_item_target.addClass(classActive);
+    }
+  );
+  
+
+
+
+});
